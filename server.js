c2d3751f079d521493481fb12401881263ef6e3b
jermiedomingo <8jermiedomingo@gmail.com>
2017-02-10 13:54:12 +0400

    web server and angular setup

---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
+
+let path = require('path');
+
+//APP CONFIGURATION ---------------
+//use body-parser so we can grab information from POST REQUESTS
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// configure our app to handle CORS requests
+app.use(function(req, res, next){
+
+	res.setHeader('Access-Control-Allow-Origin','*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+	next();
+
+});
+
+//log all requests to the console
+app.use(morgan('dev'));
+
+// set static files location
+// used for requests that our frontend will make
+app.use(express.static(__dirname + '/public'));
+
+// MAIN CATCH ALL ROUTE ---------------
+// SEND USERS TO FRONTEND ------------
+// will be handled by our angular app
+app.get('*', function(req, res) {
+res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+});
+
+// START THE SERVER
+// ===============================
+app.listen(3000);
+console.log('Magic happens on port ', 3000);
