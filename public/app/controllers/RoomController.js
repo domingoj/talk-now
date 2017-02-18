55bcdd216c600a3c30a8aa1838b3af403b915625
jermiedomingo <8jermiedomingo@gmail.com>
2017-02-18 18:26:59 +0400

    enable video and audio

--- public/app/controllers/RoomController.js
+++ public/app/controllers/RoomController.js
@@ -2,19 +2,20 @@
 
 angular.module('RoomController', [])
 
-.controller('roomController', function($scope){
+.controller('roomController',['$scope','$routeParams', function($scope, $routeParams){
 
 	let self = this;
+
+	let roomId = $routeParams.room_id;
 
 	let socket = io('http://localhost:3000' + '/room');
 
 	self.newMessage = '';
 
 	self.sendMessage = () => {
-		
 
 		socket.emit('chat message', self.newMessage);
-		$('#messages').append($('<li>').text(self.newMessage));
+		$('#messages').append($('<li>').addClass('self').text(self.newMessage));
 		self.newMessage = '';
 	}
 
@@ -24,5 +25,23 @@ angular.module('RoomController', [])
 
 	});
 
-});
+	let webrtc = new SimpleWebRTC({
+	  	// the id/element dom element that will hold "our" video
+	  	localVideoEl: 'localVideo',
+  		
+  		// the id/element dom element that will hold remote videos
+  		remoteVideosEl: 'remotesVideos',
+  		
+  		// immediately ask for camera access
+  		autoRequestMedia: true
+	});
+
+	// we have to wait until it's ready
+	webrtc.on('readyToCall', function () {
+  
+  	// you can name it anything
+  	webrtc.joinRoom(roomId);
+	});
+
+}]);
 
